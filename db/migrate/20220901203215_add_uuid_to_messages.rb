0e655c396469b74d12dfc932504c978f45a68574
Bogdanov Anton <kortirso@gmail.com>
2022-09-02 08:35:57 +0300

    check db extensions during migrations run

--- db/migrate/20220901203215_add_uuid_to_messages.rb
+++ db/migrate/20220901203215_add_uuid_to_messages.rb
@@ -1,7 +1,8 @@
 class AddUuidToMessages < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'pgcrypto'
-    enable_extension 'uuid-ossp'
+    enable_extension 'pgcrypto' unless extensions.include?('pgcrypto')
+    enable_extension 'uuid-ossp' unless extensions.include?('uuid-ossp')
+
     add_column :emailbutler_messages, :uuid, :uuid, null: false, index: true
   end
 end
